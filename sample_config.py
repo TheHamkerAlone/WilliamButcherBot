aea386c49aff3d0a476862a57a7415f6de269e27
- ⋏ Ł ꪮ ⲛ 𝛆 /𝟑 <mralone3536@gmail.com>
2025-04-24 04:13:23 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -6,7 +6,7 @@ load_dotenv(
     "config.env" if os.path.isfile("config.env") else "sample_config.env"
 )
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6392016724:AAGnr8czUSreoQ_H3Z_1h463pq2SjkrvCcw")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6392016724:")
 API_ID = os.environ.get("API_ID", "17596251")
 SESSION_STRING = os.environ.get("SESSION_STRING", "")
 API_HASH = os.environ.get("API_HASH", "e58343b4c0193e293e391daf97603fcd")
@@ -17,7 +17,7 @@ LOG_GROUP_ID = int(os.environ.get("LOG_GROUP_ID", "-1001603822916"))
 GBAN_LOG_GROUP_ID = int(os.environ.get("GBAN_LOG_GROUP_ID", "-1001603822916"))
 MESSAGE_DUMP_CHAT = int(os.environ.get("MESSAGE_DUMP_CHAT", "-1001603822916"))
 WELCOME_DELAY_KICK_SEC = int(os.environ.get("WELCOME_DELAY_KICK_SEC", 600))
-MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://hny:zara@cluster0.lfe5o.mongodb.net/?retryWrites=true&w=majority")
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://hny")
 ARQ_API_KEY = os.environ.get("ARQ_API_KEY", "QBCFLQ-GIMBVP-CZAMUW-YGDWLM-ARQ")
 ARQ_API_URL = os.environ.get("ARQ_API_URL", "https://arq.hamker.dev")
 LOG_MENTIONS = os.environ.get("LOG_MENTIONS", "True").lower() in ["true", "1"]
